784b51ba1e1faac8acf2e300596edaed4f77f52d
afred.zhou.cn@gmail.com <@Zz641645282>
2023-08-15 22:18:51 +0800

    修改ota过程

--- src/OTAPage.js
+++ src/OTAPage.js
@@ -12,13 +12,31 @@ async function getOTAFile() {
     return firmwareData;
 }
 
-function OTAPage() {
+// 计算校验和
+function calculateChecksum(data) {
+    let sum = 0;
+
+    for (let byte of data) {
+        sum += byte;
+    }
+
+    return sum;
+}
+
+function calculateAndSetChecksum(data) {
+    const checksum = calculateChecksum(data);
+    data[data.length - 1] = checksum;
+    return checksum;
+}
+function OTAPage(value) {
     const [otaFile, setOtaFile] = useState(null);
 
     useEffect(() => {
         getOTAFile().then(setOtaFile);
     }, []);
-
+    const [ACK, setACK] = useState([0x12, 0x34, 0x56]);
+    const [service, setService] = useState(null);
+    const [characteristic, setCharacteristic] = useState(null);
     const [device, setDevice] = useState(null);
     const [mtu, setMtu] = useState(20);
     const [sent, setSent] = useState(0); // 已发送数据大小
@@ -33,9 +51,20 @@ function OTAPage() {
             });
 
             setDevice(scannedDevice);
-            const server = await device.gatt.connect();
-            // const server = await navigator.bluetooth.connectGATT(scannedDevice);
+            const server = await device.gatt;
             setServer(server);
+            await device.connect();
+            await device.startNotifications();
+            await device.addEventListener('characteristicvaluechanged', (event) => {
+
+                const response = Array.from(event.target.value);
+
+                // 将响应数据设置为 ACK 状态的新值
+                setACK(response);
+                console.log(ACK.toString())
+
+            });
+
 
             // 获取MTU大小
             const mtuResp = await server.write(/* OTA start cmd */);
@@ -50,34 +79,66 @@ function OTAPage() {
 
 // 启动
     const startOTA = async () => {
-        // 构建启动命令
-        const data = Uint8Array.of(0xF1, 0x01, 0x00, 0x00, 0x00);
-        const service = await server.getPrimaryService(SERVICE_UUID);
+        const type = 0x01; // 固件下载
 
+// 总长度
+        const length = otaFile.byteLength;
+        const lengthBytes = new Uint8Array(4);
+
+        lengthBytes[0] = length;
+        lengthBytes[1] = length >> 8;
+        lengthBytes[2] = length >> 16;
+        lengthBytes[3] = length >> 24;
+
+// 构建启动命令
+        const data = new Uint8Array([
+            0xF1, // opcode
+            type,
+            ...lengthBytes, // 总长度
+            // 检验和 (省略)
+        ]);
+        // 构建启动命令
+        const checksum = calculateChecksum(data);
+        const newData = new Uint8Array([...data, checksum]);
+        const service = await server.getPrimaryService(SERVICE_UUID);
+        setService(service);
         // 获取特征值
         const characteristic = await service.getCharacteristic(CHAR_UUID);
-
+        setCharacteristic(characteristic);
+       console.log(newData);
         // 写入数据
-        await characteristic.writeValue(data);
+        await characteristic.writeValueWithoutResponse(newData);
     }
-    const sendPacket = () => {
 
-        let sent = 0;
-        const start = sent;
-        const end = Math.min(start + mtu, total);
-        const data = otaFile.slice(start, end);
 
-        // 发送数据包
-        sent += data.length;
-        setSent(sent);
+    const sendPacket = async(index, packet) => {
+
+        const data = new Uint8Array([
+            0xF2, // 传输opcode
+            index, // 2字节索引
+            ...packet // 数据
+        ]);
+
+        // 计算并设置校验和
+        calculateAndSetChecksum(data);
+        await characteristic.writeValueWithoutResponse(data);
+        // 发送data
+        setSent(sent + packet.length);
     };
 
-    const finishOTA = async () => {
-        // 构建结束命令
-        const data = Uint8Array.of(0xF3, 0x01, 0x00);
-
-        await server.write(SERVICE_UUID, CHAR_UUID, data);
+// 使用
+    const  Otaprocess = async (otaFile) => {
+        const packets = otaFile;
+        for (let i = 0; i < packets.length; i++) {
+            await sendPacket(i, packets[i]);
+        }
     }
+        const finishOTA = async () => {
+            // 构建结束命令
+            const data = Uint8Array.of(0xF3, 0x01, 0x00);
+
+            await server.writeValueWithoutResponse(data);
+        }
 
 
     useEffect(() => {
@@ -96,7 +157,7 @@ function OTAPage() {
             <div>
                 <Button variant="outlined" onClick={startScan}>Connect Device</Button>
                 <Button variant="outlined" onClick={startOTA}>startOTA</Button>
-                <Button variant="outlined" onClick={sendPacket}>OTA</Button>
+                <Button variant="outlined" onClick={Otaprocess}>OTA</Button>
                 <Button variant="outlined" onClick={finishOTA}>finishOTA</Button>
             </div>
             <progress value={sent} max={total}></progress>
